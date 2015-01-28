50f0ffdbcdc05e10e6b34d905d2fd7b4b10b3a63
Nicholas Zeman <nbzeman@gmail.com>
2015-01-28 18:16:22 -0500

    - light saber working from nunchuck

--- Assets/_script/Z_CamParent.cs
+++ Assets/_script/Z_CamParent.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Z_CamParent : MonoBehaviour {
 
 	public float mDamper = 1;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,8 +13,12 @@ public class Z_CamParent : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-
-		transform.Translate (Input.GetAxis("Horizontal")/mDamper, 0, Input.GetAxis ("Vertical")/mDamper);
-
+//		if (Wii.GetNunchuckButton(0,"C")){
+//			transform.Translate(0,0,.001f);
+//		}
+//		if (Wii.GetNunchuckButton(0,"Z")){
+//			transform.Translate(0,0,-.001f);
+//		}
+		transform.Translate (Wii.GetNunchuckAnalogStick(0).x/mDamper,0,Wii.GetNunchuckAnalogStick(0).y/mDamper);
 	}
 }
---
+++ Assets/_script/Z_fingerTrack.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Z_fingerTrack : MonoBehaviour {
+
+	public Vector3 shooterOffset;
+	public Vector3 saberOffset;
+	public Transform fingerTrans;
+	public float mult;
+	float x;
+	float y;
+	public GameObject shooter;
+	public Transform saber;
+
+	// Use this for initialization
+	void Start () {
+
+		saberOffset = new Vector3 (Wii.GetNunchuckAcceleration (0).x, Wii.GetNunchuckAcceleration (0).y, Wii.GetNunchuckAcceleration (0).z);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		shooterOffset = new Vector3 (this.transform.position.x - .35f, this.transform.position.y, this.transform.position.z);
+		fingerTrans.localPosition = new Vector3 (Wii.GetNunchuckAnalogStick (0).x, Wii.GetNunchuckAnalogStick (0).y, fingerTrans.localPosition.z);
+		lSaber ();
+	
+	}
+
+	public IEnumerator makeBall(GameObject shooter_obj){
+
+		GameObject Lshtr = Instantiate(shooter_obj,shooterOffset,Quaternion.identity) as GameObject;
+		Lshtr.transform.localEulerAngles = new Vector3(0,-12,0);
+		Lshtr.rigidbody.AddRelativeForce(0,0,2000);
+			
+		yield return new WaitForSeconds (1.4f);
+		Destroy (Lshtr);
+
+	}
+
+	public void lSaber(){
+
+		saber.localEulerAngles = new Vector3 ((Wii.GetNunchuckAcceleration (0).y - saberOffset.y) *mult, (Wii.GetNunchuckAcceleration (0).z - saberOffset.z) *mult,  (Wii.GetNunchuckAcceleration (0).x - saberOffset.x) *-mult);
+	}
+}
