cc4a54f5b9ffb2a4dfd4d87fc5caecd38edfc523
Nicholas Zeman <nbzeman@gmail.com>
2015-01-29 17:35:06 -0500

    - got the full frustum completed!! - set up auto-switches for various rigs using enums

--- Assets/_script/Z_CamParent.cs
+++ Assets/_script/Z_CamParent.cs
@@ -4,21 +4,25 @@ using System.Collections;
 public class Z_CamParent : MonoBehaviour {
 
 	public float mDamper = 1;
-
+	public Vector2 nunchuckOffset;
 	// Use this for initialization
 	void Start () {
 	
+		nunchuckOffset = new Vector2 (Wii.GetNunchuckAnalogStick (1).x, Wii.GetNunchuckAnalogStick (1).y);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		print (Wii.GetNunchuckAnalogStick (1).y);
 //		if (Wii.GetNunchuckButton(0,"C")){
 //			transform.Translate(0,0,.001f);
 //		}
 //		if (Wii.GetNunchuckButton(0,"Z")){
 //			transform.Translate(0,0,-.001f);
 //		}
-		transform.Translate (Wii.GetNunchuckAnalogStick(0).x/mDamper,0,Wii.GetNunchuckAnalogStick(0).y/mDamper);
+
+			transform.Translate (Wii.GetNunchuckAnalogStick(1).x/mDamper,0,Wii.GetNunchuckAnalogStick(1).y/mDamper);
 	}
 }
--- Assets/_script/Z_wiiTrackLee.cs
+++ Assets/_script/Z_wiiTrackLee.cs
@@ -20,6 +20,9 @@ public class Z_wiiTrackLee : MonoBehaviour {
 	//vectors from the wii IR lights
 	public GameObject leftIR;
 	public GameObject rightIR;
+	public Transform bleft;
+	public Transform topleft;
+	public Transform bright;
 	public Vector2 firstPoint = new Vector2();//first IR Point XY Position
 	public Vector2 secondPoint = new Vector2();//second IR Point XY Position
 	int numvisible = 0;
@@ -28,6 +31,8 @@ public class Z_wiiTrackLee : MonoBehaviour {
 	public int pixel_width = 1680;
 	public int pixel_height = 1050;
 	private float screenAspect;
+	//switches for perspective stuff
+	public bool CAVE = false;
 	public bool lookatswitch = true;
 	public bool FOV = false;
 	public bool Matrix = false;
@@ -59,14 +64,38 @@ public class Z_wiiTrackLee : MonoBehaviour {
 
 		TrackHead ();
 		if(Matrix){//only do this if the user chooses, for experimenting with getting the perspective correct
-			Matrix4x4 m = PerspectiveOffCenter (near*(-.5f * screenAspect + mHeadX)/mHeadDist,//left
-			                                    near*(.5f * screenAspect + mHeadX)/mHeadDist,//right
-			                                    near*(-.5f + mHeadY)/mHeadDist,//bottom
-			                                    near*(.5f + mHeadY)/mHeadDist,//top
+
+
+			Matrix4x4 m = PerspectiveOffCenter (
+												near*(-.5f * screenAspect + mHeadX)* mHeadDist,//left
+			                                    near*(.5f * screenAspect + mHeadX)* mHeadDist,//right
+			                                    near*(-.5f - mHeadY) * mHeadDist,//bottom
+			                                    near*(.5f - mHeadY) * mHeadDist,//top
 			                                    near,//near
-			                                    far//far
+			                                    100//far
 			                                    );
+
 			HeadCam.projectionMatrix = m;
+			HeadCam.projectionMatrix = HeadCam.projectionMatrix * Matrix4x4.Scale(new Vector3(1,1,1));
+		}
+		if(CAVE){
+
+			Vector3 BottomLeftCorner = new Vector3(0 - mScreenWidthInMM/200, 0 - mScreenHeightInMM/100, -mHeadDist);
+			Vector3 BottomRightCorner = new Vector3(0 + mScreenWidthInMM/200, 0 - mScreenHeightInMM/100, -mHeadDist);
+			Vector3 TopLeftCorner = new Vector3(0 - mScreenWidthInMM/200, 0, -mHeadDist);
+			Vector3 trackerPosition = mHeadPosition;
+
+
+			Matrix4x4 genProjection = GeneralizedPerspectiveProjection(
+				BottomLeftCorner,//0 - screenWidth/2, currentHeight - screen Height, mHeadDist
+				BottomRightCorner,//0 + screenWidth/2, currentHeight - screen Height, mHeadDist
+				TopLeftCorner,//0-screenWidth/2, 0, mHeadDist 
+				trackerPosition,//mHeadX, mHeadY, -mHeadDist
+				.05f,
+				100
+				);
+			HeadCam.projectionMatrix = genProjection;  
+
 		}
 
 	
@@ -100,22 +129,23 @@ public class Z_wiiTrackLee : MonoBehaviour {
 		float relativeVerticalAngle = (avgY - 384) * mRadiansPerPixel;
 
 		if(mWiiMoteIsAboveScreen){
-			mHeadY = .5f + Mathf.Sin(relativeVerticalAngle + mWiiMoteVerticleAngle) *mHeadDist;
+			mHeadY = .5f + Mathf.Sin(relativeVerticalAngle + mWiiMoteVerticleAngle) * mHeadDist;
 
 		}else{
 			mHeadY = -.5f + Mathf.Sin(relativeVerticalAngle + mWiiMoteVerticleAngle) * mHeadDist;
 		}
 
 		//now apply that crap to the camera transforms
-		Vector3 newHeadPosition = new Vector3 (mHeadX*gmult, mHeadY*gmult, -mHeadDist*gmult);
-		HeadCam.transform.localPosition = newHeadPosition;
-		Vector3 lookAt = new Vector3(mHeadX/1.1f , mHeadY/1.1f , 0);//lookat point, currently being fakes      
-		lookAtObject.localPosition = new Vector3 (mHeadX*gmult / 1.1f, mHeadY*gmult / 1.1f, 0);
+		//Vector3 newHeadPosition = new Vector3 (mHeadX, mHeadY, -mHeadDist);
+		HeadCam.transform.localPosition = new Vector3 (mHeadX, mHeadY, -mHeadDist);
+		//Vector3 lookAt = new Vector3(mHeadX , mHeadY , 0);//lookat point, currently being fakes      
+		lookAtObject.localPosition = new Vector3 (0, mHeadY, 1);
 
 
 		if (lookatswitch){//makes head cam look at lookat point when switched on
 			HeadCam.transform.LookAt (lookAtObject);
 		}
+
 
 		//the following is Johnny Lee's Projection Matrix
 		/*float nearPlane = .05f;
@@ -130,19 +160,6 @@ public class Z_wiiTrackLee : MonoBehaviour {
 			
 			);*/
 
-		//the following is from the Ogre 3D guy
-		if (FOV){//turns on FOV Projection Matrix stuff
-
-			FOV1 = (107 - .1944f * mHeadDist * mScreenHeightInMM/10) * FOVMult;
-
-			float designedScreenWidth = mScreenWidthInMM; // I built my UI based on a 640px wide screen
-			float distanceToUI = mHeadDist; // My UI elements are 500 units from the perspective camera
-			float hFOVrad = Mathf.Atan((designedScreenWidth/2f)/distanceToUI);
-			float aspect = Screen.width/Screen.height; // capture the CURRENT camera aspect ratio
-			float vFOVdeg = Mathf.Atan(Mathf.Tan(hFOVrad/2f)/aspect) * 2f * Mathf.Rad2Deg;
-			HeadCam.fieldOfView = FOV1;
-		}
-
 	}
 
 	static Matrix4x4 PerspectiveOffCenter(
@@ -150,60 +167,160 @@ public class Z_wiiTrackLee : MonoBehaviour {
 		float bottom, float top,
 		float near, float far )
 	{    
-		//unity's projection matrix
+		//unity's projection matrix original
 		float x =  (2.0f * near) / (right - left);
 		float y =  (2.0f * near) / (top - bottom);
 		float a =  (right + left) / (right - left);
 		float b =  (top + bottom) / (top - bottom);
-		float c = -(far + near) / (far - near);
-		float d = -(2.0f * far * near) / (far - near);
+		float c = - (far + near) / (far - near);
+		float d = -(2.0f * far * near )/ (far - near);
 		float e = -1.0f;
+
+		//D3DXMatrix values
+//		float x =  (2.0f * near) / (right - left);
+//		float y =  (2.0f * near) / (top - bottom);
+//		float a =  (left + right) / (left - right);
+//		float b =  (top + bottom) / (bottom-top);
+//		float c =  (far) / (far - near);
+//		float d =  (far * near) / (near - far);
+//		float e =  1.0f;
+		
 		
 		Matrix4x4 m = new Matrix4x4 ();
-		m[0,0] = x;  m[0,1] = 0;  m[0,2] = a;  m[0,3] = 0;
-		m[1,0] = 0;  m[1,1] = y;  m[1,2] = b;  m[1,3] = 0;
-		m[2,0] = 0;  m[2,1] = 0;  m[2,2] = c;  m[2,3] = d;
-		m[3,0] = 0;  m[3,1] = 0;  m[3,2] = e;  m[3,3] = 0;
+
+		//orig from Unity
+//		m[0,0] = x;
+//		m[0,1] = 0;
+//		m[0,2] = a;
+//		m[0,3] = 0;
+//		m[1,0] = 0;
+//		m[1,1] = y;
+//		m[1,2] = b;
+//		m[1,3] = 0;
+//		m[2,0] = 0;
+//		m[2,1] = 0;
+//		m[2,2] = c;
+//		m[2,3] = d;
+//		m[3,0] = 0;
+//		m[3,1] = 0;
+//		m[3,2] = e;
+//		m[3,3] = 0;
+
+		m[0,0] = x;
+		m[0,1] = 0;
+		m[0,2] = a;
+		m[0,3] = 0;
+					m[1,0] = 0;
+					m[1,1] = y;
+					m[1,2] = b;
+					m[1,3] = 0;
+								m[2,0] = 0;
+								m[2,1] = 0;
+								m[2,2] = c;
+								m[2,3] = d;
+											m[3,0] = 0;
+											m[3,1] = 0;
+											m[3,2] = e;
+											m[3,3] = 0;
+										
 		
 		return m;
 		
 	}
 
+	//CAVE guy stuff
+	public static Matrix4x4 GeneralizedPerspectiveProjection(Vector3 pa, Vector3 pb, Vector3 pc, Vector3 pe, float near, float far)
 
+	{
+		Vector3 va, vb, vc;
+		Vector3 vr, vu, vn;
+		
+		float left, right, bottom, top, eyedistance;
+		
+		Matrix4x4 transformMatrix;
+		Matrix4x4 projectionM;
+		Matrix4x4 eyeTranslateM;
+		Matrix4x4 finalProjection;
+		
+		///Calculate the orthonormal for the screen (the screen coordinate system
+		vr = pb - pa;
+		vr.Normalize();
+		vu = pc - pa;
+		vu.Normalize();
+		vn = Vector3.Cross(vr, vu);
+		vn.Normalize();
+		
+		//Calculate the vector from eye (pe) to screen corners (pa, pb, pc)
+		va = pa-pe;
+		vb = pb-pe;
+		vc = pc-pe;
+		
+		//Get the distance;; from the eye to the screen plane
+		eyedistance = -(Vector3.Dot(va, vn));
+		
+		//Get the varaibles for the off center projection
+		left = (Vector3.Dot(vr, va)*near)/eyedistance;
+		right  = (Vector3.Dot(vr, vb)*near)/eyedistance;
+		bottom  = (Vector3.Dot(vu, va)*near)/eyedistance;
+		top = (Vector3.Dot(vu, vc)*near)/eyedistance;
+		
+		//Get this projection
+		projectionM = PerspectiveOffCenter(left, right, bottom, top, near, far);
+		
+		//Fill in the transform matrix
+		transformMatrix = new Matrix4x4();
+		transformMatrix[0, 0] = vr.x;
+		transformMatrix[0, 1] = vr.y;
+		transformMatrix[0, 2] = vr.z;
+		transformMatrix[0, 3] = 0;
+		transformMatrix[1, 0] = vu.x;
+		transformMatrix[1, 1] = vu.y;
+		transformMatrix[1, 2] = vu.z;
+		transformMatrix[1, 3] = 0;
+		transformMatrix[2, 0] = vn.x;
+		transformMatrix[2, 1] = vn.y;
+		transformMatrix[2, 2] = vn.z;
+		transformMatrix[2, 3] = 0;
+		transformMatrix[3, 0] = 0;
+		transformMatrix[3, 1] = 0;
+		transformMatrix[3, 2] = 0;
+		transformMatrix[3, 3] = 1;
+		
+		//Now for the eye transform
+		eyeTranslateM = new Matrix4x4();
+		eyeTranslateM[0, 0] = 1;
+		eyeTranslateM[0, 1] = 0;
+		eyeTranslateM[0, 2] = 0;
+		eyeTranslateM[0, 3] = -pe.x;
+		eyeTranslateM[1, 0] = 0;
+		eyeTranslateM[1, 1] = 1;
+		eyeTranslateM[1, 2] = 0;
+		eyeTranslateM[1, 3] = -pe.y;
+		eyeTranslateM[2, 0] = 0;
+		eyeTranslateM[2, 1] = 0;
+		eyeTranslateM[2, 2] = 1;
+		eyeTranslateM[2, 3] = -pe.z;
+		eyeTranslateM[3, 0] = 0;
+		eyeTranslateM[3, 1] = 0;
+		eyeTranslateM[3, 2] = 0;
+		eyeTranslateM[3, 3] = 1f;
+		
+		//Multiply all together
+		finalProjection = new Matrix4x4();
+		finalProjection = Matrix4x4.identity * projectionM*transformMatrix*eyeTranslateM;
+		
+		//finally return
+		return finalProjection;
+	}
+
+	//test matrix, probably doesn't work
+//	float scaleFov = (1+headPos.z/100)/(1+lastHeadPos.z/100);  //Calculate de zoom: dividing by lastHeadPos becouse the current zoom will be accumulated on de currentMatrix of the camera
+//	Matrix4 headMatrix = Matrix4( 
+//	                             scaleFov, 0, headDelta.x, headDelta.x*zNear,
+//	                             0, scaleFov, headDelta.y, headDelta.y*zNear,
+//	                             0, 0,  scaleFov,  0,
+//	                             0, 0, 0, 1 );
 }
 
-//var startPosition : Vector3;
-//var currentPosition : Vector3;
-//
-//
-//function Start () {
-//	startPosition = Camera.main.transform.position;
-//	var r : Rect = Camera.main.pixelRect;
-//	Debug.Log(r);
-//}
-//
-//
-//function LateUpdate () {
-//	
-//	var cam : Camera = camera;
-//	cam.farClipPlane = 20;
-//	cam.nearClipPlane = 0.5;
-//	//camera.transform.LookAt(GameObject.Find("room").transform.position, Vector3.up);
-//	//  camera.transform.LookAt(Vector3(cam.transform.position.x, cam.transform.position.y, 0), Vector3.down);
-//	
-//	var left : float =  cam.nearClipPlane * (-0.5 * 16 + cam.transform.position.x) / cam.transform.position.z;
-//	var right : float = cam.nearClipPlane * (0.5 * 16 + cam.transform.position.x) / cam.transform.position.z;
-//	var bottom : float = cam.nearClipPlane * (-0.5 * 9 + cam.transform.position.y) / cam.transform.position.z;
-//	var top : float = cam.nearClipPlane * (0.5 * 9 + cam.transform.position.y) / cam.transform.position.z;
-//	
-//	
-//	var m : Matrix4x4 = PerspectiveOffCenter(
-//		left, right, bottom, top,
-//		cam.nearClipPlane, 20 );  
-//	
-//	cam.projectionMatrix = m;
-//	Debug.Log(m);
-//	
-//}
-//
+
 
