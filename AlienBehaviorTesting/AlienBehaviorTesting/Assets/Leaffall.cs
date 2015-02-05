83aaadc4faf521e30207e31a73b209bc19856925
HunterOka <okah1@nku073088.hh.nku.edu>
2015-02-04 20:16:40 -0500

    Create alien test behavior project

---
+++ AlienBehaviorTesting/AlienBehaviorTesting/Assets/Leaffall.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class Leaffall : MonoBehaviour {
+
+    private bool leftTop = false;
+    private bool rightTop = false;
+    private float arcAmount = 5.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        
+        
+	
+	}
+}
